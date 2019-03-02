3c24862c6fc357327d30a083140bd06f6a5181c7
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-03-02 18:43:28 +0530

    port changed to PORT

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("./startup/routes")(app);
 
 app.use(express.static(__dirname + "/public"));
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
